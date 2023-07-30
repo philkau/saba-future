42910aa3ef9a12a7fb2de3c0125b9b9a510fb587
philkau <elsevierphil@gmail.com>
2023-07-31 00:26:30 +0800

    Update with PredictionWorld7

--- nextjs/pages/test.js
+++ nextjs/pages/test.js
@@ -6,6 +6,31 @@ import { IconButton } from "@mui/material";
 import { ethers } from "ethers";
 import { useCallback, useEffect, useState } from "react";
 
+const MARKET_FILTERING_STATUS = {
+    ALL: 0,
+    NOT_CLOSED: 1,
+    CLOSED: 2
+};
+
+const MARKET_FILTERING_WITH_TEST = {
+    YES: true,
+    NO: false
+};
+
+const MARKET_ORDER_TYPE = {
+    ASC: false,
+    DESC: true
+};
+
+const PAGE_SIZE = 5;
+
+// // Mumbai
+// const PROVIDER = new ethers.providers.JsonRpcProvider("https://polygon-mumbai.g.alchemy.com/v2/zuA4YikAUhYUlQw2PpucJx1jsv5eVysb");
+// Mainnet
+const PROVIDER = new ethers.providers.JsonRpcProvider("https://polygon-mainnet.g.alchemy.com/v2/B8ncZIIjNn8eul-QPcOcgBac3pFdOH6_");
+
+const PREDICTION_WORLD_CONTRACT = new ethers.Contract(predictionWorldAddress, PredictionWorld.abi, PROVIDER);
+
 export default function Test() {
     const [markets, setMarkets] = useState([]);
     const [nextCursor, setNextCursor] = useState(0);
@@ -14,26 +39,34 @@ export default function Test() {
 
     const fetchData = useCallback(async (cursor) => {
         setLoading(true);
-        const size = 10;
-        const provider = new ethers.providers.JsonRpcProvider("https://polygon-mainnet.g.alchemy.com/v2/B8ncZIIjNn8eul-QPcOcgBac3pFdOH6_");
-        const predictionWorldContract = new ethers.Contract(predictionWorldAddress, PredictionWorld.abi, provider);
-        const response = await predictionWorldContract.fetchMarkets(cursor, size, true);
+
+        const response = await PREDICTION_WORLD_CONTRACT.fetchMarkets(
+            cursor,
+            PAGE_SIZE,
+            MARKET_FILTERING_STATUS.NOT_CLOSED,
+            MARKET_FILTERING_WITH_TEST.YES,
+            MARKET_ORDER_TYPE.DESC
+        );
         const nextMarkets = response[0];
         const nextCursor = response[1];
 
         setMarkets((pre) => [...pre, ...nextMarkets]);
         setNextCursor(nextCursor);
-        setFinished(nextMarkets.length < size);
+        setFinished(nextMarkets.length < PAGE_SIZE);
         setLoading(false);
     }, []);
 
     useEffect(() => {
-        fetchData(0);
+        const fetchFirst = async () => {
+            const total = await PREDICTION_WORLD_CONTRACT.totalMarkets();
+            await fetchData(total);
+        };
+        fetchFirst();
     }, [fetchData]);
 
     return (
         <>
-            <div>Fetched Markets: {markets.length}</div>
+            <div>Open Markets: {markets.length}</div>
             <IconButton disabled={loading || finished} onClick={() => fetchData(nextCursor)} size="small" aria-label="get more">
                 More <ReadMoreIcon fontSize="large" />
             </IconButton>
